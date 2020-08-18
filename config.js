f7a1f66cd9eaf482e7b81abe8126a5ae4963b564
maoxt <27711210@qq.com>
2020-08-19 02:33:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'proxy.netptop.com',
+    serverName: 'siteproxy11.herokuapp.com',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
